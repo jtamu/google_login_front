b7c85a87eac34f3bb8a3aaf14e6a58113a3cf385
jtamu <gtamu.dip@gmail.com>
2024-03-20 00:55:55 +0900

    feat: 認可エンドポイントにアクセス

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	function login() {
+		const params = {
+			response_type: 'code',
+			client_id: '720487762083-hbm6v2q5loampnkcsicc0d0303lsksom.apps.googleusercontent.com',
+			scope: 'openid',
+			redirect_uri: 'https://google-login.jtamu-sample-app.link/callback',
+			state: Math.random().toString(32).substring(2),
+			nonce: Math.random().toString(32).substring(2)
+		};
+		const href = `https://accounts.google.com/o/oauth2/v2/auth?response_type=${params.response_type}&client_id=${params.client_id}&scope=${params.scope}&redirect_uri=${params.redirect_uri}&state=${params.state}&nonce=${params.nonce}`;
+		location.href = href;
+	}
+</script>
+
+<h1>ログイン</h1>
+<p><button on:click={login}>Googleでログイン</button></p>
