ac60b6c1aae90fd031a879f23d653e144f0bedcc
jtamu <gtamu.dip@gmail.com>
2024-03-22 23:54:07 +0900

    feat: get token

--- src/routes/callback/+page.svelte
+++ src/routes/callback/+page.svelte
@@ -1,19 +1,23 @@
 <script lang="ts">
 	import { error } from '@sveltejs/kit';
+	import axios from 'axios';
 	import { onMount } from 'svelte';
 	let queryParams;
 
-	onMount(() => {
+	onMount(async () => {
 		const params = new URLSearchParams(window.location.search);
 		queryParams = Object.fromEntries(params.entries());
-		console.log(queryParams);
 
 		const sessionState = sessionStorage.getItem('state');
-		console.log(sessionState);
 
 		if (queryParams.state != sessionState) {
 			error(401, { message: 'Invalid state parameter.' });
 		}
+
+		const res = await axios.post('https://nfk13r40e6.execute-api.ap-northeast-1.amazonaws.com/api/get_token', {
+			code: queryParams.code,
+		});
+		console.log(res);
 	});
 </script>
 
