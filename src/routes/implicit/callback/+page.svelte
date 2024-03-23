c21906688494daf4f80ee39eae7c1d4a56730487
jtamu <gtamu.dip@gmail.com>
2024-03-24 01:06:12 +0900

    feat: 不要なlog削除

--- src/routes/implicit/callback/+page.svelte
+++ src/routes/implicit/callback/+page.svelte
@@ -6,7 +6,6 @@
 
     onMount(async () => {
         const fragment = window.location.hash;
-        console.log(fragment);
         const keyValueArgs = fragment.substring(1).split('&');
         let fragmentArgs: Record<string, string> = {};
         for (const keyValue of keyValueArgs) {
