08eba65570914ae0d136e233b07fd68b6d8d6187
jtamu <gtamu.dip@gmail.com>
2024-03-24 00:45:48 +0900

    feat: get userinfo (implicit)

--- src/routes/implicit/callback/+page.svelte
+++ src/routes/implicit/callback/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+	import axios, { type AxiosResponse } from "axios";
 	import { onMount } from "svelte";
+	let userInfoRes: AxiosResponse;
 
-    onMount(() => {
+    onMount(async () => {
         const fragment = window.location.hash;
         console.log(fragment);
         const keyValueArgs = fragment.substring(1).split('&');
@@ -10,8 +12,17 @@
             const [key, value] = keyValue.split('=');
             fragmentArgs[decodeURIComponent(key)] = decodeURIComponent(value);
         }
-        console.log(fragmentArgs);
+
+        // userinfoエンドポイントにアクセス
+		const discoverDoc = JSON.parse(sessionStorage.getItem('discoverDoc') ?? '');
+		userInfoRes = await axios.get(discoverDoc.userinfo_endpoint, {headers: {Authorization: `Bearer ${fragmentArgs.access_token}`}});
+		console.log(userInfoRes);
     });
 </script>
 
-<h1>Callback(Implicit)</h1>
+{#if userInfoRes}
+	<h1>{userInfoRes.data?.name}</h1>
+	<img src={userInfoRes.data?.picture} alt="facePicture" />
+{:else}
+	<h1>ログインしています...</h1>
+{/if}
